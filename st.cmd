270a4bf47c4746bc20146d76ccf8676a0f0fb344
zhaoshutao <zhaost@ihep.ac.cn>
2024-06-25 19:55:12 +0800

    2024.06.25-full version

--- st.cmd
+++ st.cmd
@@ -9,14 +9,19 @@ opcSetServer("1","opcda:///BECKHOFF.TwinCATOpcServerDA/{F23C1FFA-2396-4BA7-8EA3-
 # OpcGroup#1 has instance ID 1
 # OpcGroup#1 has update rate of 20 milliseconds
 # OpcGroup#1 is connected to server instance with ID 1
-opcSetGroup("1","OpcGroup#1","20", "1")
+opcSetGroup("1","OpcGroup#1","10", "1")
 
 # OpcGroup#2 has instance ID 2
 # OpcGroup#2 has update rate of 50 milliseconds
 # OpcGroup#2 is connected to server instance with ID 1
-#opcSetGroup("2","OpcGroup#2","50", "1")
+opcSetGroup("2","OpcGroup#2","50", "1")
 
-dbLoadRecords("C:\HZB\OpcIocShell\IDControlIOC\IDControlRecord1.db")
+# OpcGroup#2 has instance ID 2
+# OpcGroup#2 has update rate of 100 milliseconds
+# OpcGroup#2 is connected to server instance with ID 1
+opcSetGroup("3","OpcGroup#2","100", "1")
 
+dbLoadRecords("C:\HZB\OpcIocShell\IDControlIOC\R02IAU.db")
+#dbLoadTemplate("C:\HZB\OpcIocShell\IDControlIOC\IDControl.substitutions")
 iocInit()
 # let there always be a line after iocInit()
